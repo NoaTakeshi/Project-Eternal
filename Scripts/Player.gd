04fd85993d4646010e3e4cd5726751029c10a78d
NoaTakeshi <pipepaipei2014@gmail.com>
2025-11-10 20:10:32 -0500

    player ya semueve

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,11 +1,39 @@
-extends Node
+extends CharacterBody2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+const SPEED = 300.0
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+
+func _physics_process(delta):
+	
+	var dir: Vector2 = Input.get_vector("left","rigth","up","down")
+	
+	
+	if dir:
+		
+		if dir.x != 0:
+			if dir.x > 0:
+				$AnimatePlayer.play("Caminar_derecha")
+				pass
+			else:
+				$AnimatePlayer.play("Caminar_izquierda")
+			pass
+			
+		else:
+			if dir.y > 0:
+				$AnimatePlayer.play("Caminar_abajo")
+				pass
+			else:
+				$AnimatePlayer.play("Caminar_Arriba")
+			pass
+			
+		pass
+	else:
+		$AnimatePlayer.stop()
+		pass
+		
+	velocity = dir.normalized() * SPEED
+	move_and_slide()
+		
 	pass
